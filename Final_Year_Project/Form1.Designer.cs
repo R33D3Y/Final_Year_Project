0ff1de6c25d50a31bd1c03abd8c40ccaaaa537aa
R33D3Y <reedey2708@gmail.com>
2018-10-07 13:42:04 +0100

    Base calendar asthetics working

--- Final_Year_Project/Form1.Designer.cs
+++ Final_Year_Project/Form1.Designer.cs
@@ -28,160 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
-            this.tableLayoutPanel1.ColumnCount = 7;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel1.Controls.Add(this.label7, 6, 0);
-            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.label5, 4, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label6, 5, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label4, 3, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
-            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(58, 60);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 11;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(696, 430);
-            this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 7);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Monday";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(78, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Tuesday";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(143, 11);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Wednesday";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(218, 11);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 13);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Thursday";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(294, 10);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "Friday";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(357, 9);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(49, 13);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Saturday";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(433, 10);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(43, 13);
-            this.label7.TabIndex = 2;
-            this.label7.Text = "Sunday";
-            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(0, 62);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(70, 41);
-            this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "Jack - Shopping";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(861, 614);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(713, 480);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Final_Year_Project/Form1.cs
+++ Final_Year_Project/Form1.cs
@@ -1,20 +1,133 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Final_Year_Project
 {
     public partial class Form1 : Form
     {
+        private Calendar calendar;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            calendar = new Calendar(DateTime.Now);
+            calendar.Render();
+            Controls.Add(calendar.GetCalendar());
+        }
+    }
+
+    public class Calendar
+    {
+        DateTime start;
+        TableLayoutPanel calendar;
+
+        public Calendar(DateTime s)
+        {
+            start = s;
+            calendar = new TableLayoutPanel();
+        }
+
+        public void Render()
+        {
+            calendar.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            calendar.ColumnCount = 7;
+            calendar.RowCount = 1;
+
+            calendar.AutoSize = true;
+
+            calendar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 70));
+            calendar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 70));
+            calendar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 70));
+            calendar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 70));
+            calendar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 70));
+            calendar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 70));
+            calendar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 70));
+
+            calendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 30));
+            calendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 20));
+            calendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 60));
+            calendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 20));
+            calendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 60));
+            calendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 20));
+            calendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 60));
+            calendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 20));
+            calendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 60));
+            calendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 20));
+            calendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 60));
+
+            calendar.Controls.Add(new Label() { Text = "Monday" }, 0, 0);
+            calendar.Controls.Add(new Label() { Text = "Tuesday" }, 1, 0);
+            calendar.Controls.Add(new Label() { Text = "Wednesday" }, 2, 0);
+            calendar.Controls.Add(new Label() { Text = "Thursday" }, 3, 0);
+            calendar.Controls.Add(new Label() { Text = "Friday" }, 4, 0);
+            calendar.Controls.Add(new Label() { Text = "Saturday" }, 5, 0);
+            calendar.Controls.Add(new Label() { Text = "Sunday" }, 6, 0);
+
+            calendar.Controls.Add(new Label() { Text = "1st" }, 0, 1);
+            calendar.Controls.Add(new Label() { Text = "2nd" }, 1, 1);
+            calendar.Controls.Add(new Label() { Text = "3rd" }, 2, 1);
+            calendar.Controls.Add(new Label() { Text = "4th" }, 3, 1);
+            calendar.Controls.Add(new Label() { Text = "6th" }, 4, 1);
+            calendar.Controls.Add(new Label() { Text = "7th" }, 5, 1);
+            calendar.Controls.Add(new Label() { Text = "8th" }, 6, 1);
+
+            calendar.Controls.Add(new Label() { Text = "" }, 0, 2);
+            calendar.Controls.Add(new Label() { Text = "Football" }, 1, 2);
+            calendar.Controls.Add(new Label() { Text = "" }, 2, 2);
+            calendar.Controls.Add(new Label() { Text = "Meeting" }, 3, 2);
+            calendar.Controls.Add(new Label() { Text = "" }, 4, 2);
+            calendar.Controls.Add(new Label() { Text = "" }, 5, 2);
+            calendar.Controls.Add(new Label() { Text = "Shopping" }, 6, 2);
+
+            calendar.Controls.Add(new Label() { Text = "9th" }, 0, 3);
+            calendar.Controls.Add(new Label() { Text = "10th" }, 1, 3);
+            calendar.Controls.Add(new Label() { Text = "11th" }, 2, 3);
+            calendar.Controls.Add(new Label() { Text = "12th" }, 3, 3);
+            calendar.Controls.Add(new Label() { Text = "13th" }, 4, 3);
+            calendar.Controls.Add(new Label() { Text = "14th" }, 5, 3);
+            calendar.Controls.Add(new Label() { Text = "15th" }, 6, 3);
+
+            calendar.Controls.Add(new Label() { Text = "" }, 0, 4);
+            calendar.Controls.Add(new Label() { Text = "" }, 1, 4);
+            calendar.Controls.Add(new Label() { Text = "Birthday" }, 2, 4);
+            calendar.Controls.Add(new Label() { Text = "Meeting" }, 3, 4);
+            calendar.Controls.Add(new Label() { Text = "" }, 4, 4);
+            calendar.Controls.Add(new Label() { Text = "Laundry" }, 5, 4);
+            calendar.Controls.Add(new Label() { Text = "" }, 6, 4);
+        }
+
+        public void AddEvent()
+        {
+
+        }
+
+        public void RemoveEvent()
+        {
+
+        }
+
+        public void NextWeek()
+        {
+
+        }
+
+        public void PreviousWeek()
+        {
+
+        }
+
+        public void Sync()
+        {
+
+        }
+
+        public TableLayoutPanel GetCalendar()
+        {
+            return calendar;
         }
     }
 }
